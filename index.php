7ea2c10f104ff0c75c1a5b97d19193ca46c9a73a
brunEdo <edobruno.m@gmail.com>
2021-11-02 17:18:37 +0100

    movies array

--- index.php
+++ index.php
@@ -1,13 +1,36 @@
 <?php
 require __DIR__ . '/partials/Classes.php';
 
-$bladeRunner2049 = new Movie (
-  "Blade Runner 2049",
-  "Denis",
-  ["Ryan Gosling", "Harrison Ford", "Jared Leto"],
-  "Sci-fi");
+$movies = [
+  "bladeRunner2049" => new Movie (
+    "Blade Runner 2049",
+    "Denis",
+    ["Ryan Gosling", "Harrison Ford", "Jared Leto"],
+    "Sci-fi"),
+  "dune" => new Movie (
+    "Dune: Part One",
+    "Denis Villeneuve",
+    ["Timothée Chalamet", "Rebecca Ferguson", "Jason Momoa"],
+    "Sci-fi")
+  ];
 
-echo $bladeRunner2049->get("author");
-echo $bladeRunner2049->set("author", "Denis Villeneuve");
-echo $bladeRunner2049->get("author");
+$movies["bladeRunner2049"]->set("author", "Denis Villeneuve");
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <?php foreach($movies as $movie) {?>
+  <h2><?= $movie->get("name") ?></h2>
+  <p>Genre: <?= $movie->get("genre") ?></p>
+  <p>Author: <?= $movie->get("author") ?></p>
+  <p>Cast: <?php foreach ($movie->get("cast") as $actor) echo "{$actor}, " ?></p>
+  <?php } ?>
+</body>
+</html>
